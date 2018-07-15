ef7360c492cd933e41e11d49be7e49d980a9dc37
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2018-07-15 09:55:08 +0100

    Solved polluted namspaces in BigDecimalExtension

--- src/main/scala/hu/szigyi/timelapse/ramping/math/BigDecimalExtension.scala
+++ src/main/scala/hu/szigyi/timelapse/ramping/math/BigDecimalExtension.scala
@@ -1,35 +1,37 @@
 package hu.szigyi.timelapse.ramping.math
 
-import java.math.MathContext
+import java.math.{MathContext, BigDecimal => JavaBigDecimal, RoundingMode => JavaRoundingMode}
 
 import scala.math.BigDecimal.RoundingMode.RoundingMode
 
 object MathContext {
-  def apply(precision: Int, roundMode: java.math.RoundingMode) = new MathContext(precision, roundMode)
+  def apply(precision: Int, roundMode: JavaRoundingMode) = new MathContext(precision, roundMode)
 }
 
 object BigDecimalConverter {
   import math.BigDecimal._
-  implicit def convertToJava(bigDecimal: BigDecimal): java.math.BigDecimal = bigDecimal.bigDecimal
-  implicit def convertFromInt(int: Int): java.math.BigDecimal = new java.math.BigDecimal(int, defaultMathContext)
+  implicit def convertToJava(bigDecimal: BigDecimal): JavaBigDecimal = bigDecimal.bigDecimal
+  implicit def convertFromInt(int: Int): JavaBigDecimal = new JavaBigDecimal(int, defaultMathContext)
 }
 
 object BigDecimalEquals {
+
   import math.BigDecimal._
+
   def bdEquals(bd1: BigDecimal, bd2: BigDecimal): Boolean = {
     val scaledBD1 = bd1.setScale(defaultMathContext.getPrecision, defaultMathContext.getRoundingMode)
     val scaledBD2 = bd2.setScale(defaultMathContext.getPrecision, defaultMathContext.getRoundingMode)
     scaledBD1.equals(scaledBD2)
   }
 
-  private implicit def roundingModeConverter(roundingMode: java.math.RoundingMode): RoundingMode = roundingMode match {
-    case java.math.RoundingMode.CEILING => RoundingMode.CEILING
-    case java.math.RoundingMode.DOWN => RoundingMode.DOWN
-    case java.math.RoundingMode.FLOOR => RoundingMode.FLOOR
-    case java.math.RoundingMode.HALF_DOWN => RoundingMode.HALF_DOWN
-    case java.math.RoundingMode.HALF_EVEN => RoundingMode.HALF_EVEN
-    case java.math.RoundingMode.HALF_UP => RoundingMode.HALF_UP
-    case java.math.RoundingMode.UNNECESSARY => RoundingMode.UNNECESSARY
-    case java.math.RoundingMode.UP => RoundingMode.UP
+  private implicit def roundingModeConverter(roundingMode: JavaRoundingMode): RoundingMode = roundingMode match {
+    case JavaRoundingMode.CEILING => RoundingMode.CEILING
+    case JavaRoundingMode.DOWN => RoundingMode.DOWN
+    case JavaRoundingMode.FLOOR => RoundingMode.FLOOR
+    case JavaRoundingMode.HALF_DOWN => RoundingMode.HALF_DOWN
+    case JavaRoundingMode.HALF_EVEN => RoundingMode.HALF_EVEN
+    case JavaRoundingMode.HALF_UP => RoundingMode.HALF_UP
+    case JavaRoundingMode.UNNECESSARY => RoundingMode.UNNECESSARY
+    case JavaRoundingMode.UP => RoundingMode.UP
   }
 }
