ed871cc575e04d2aab10ba8a49e89dd832853b7f
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2018-07-26 00:24:27 +0100

    Added tests against the basic Equations. Improved documentation of equations. Moved BigDecimal features under BigDecimalDecorator

--- src/main/scala/hu/szigyi/timelapse/ramping/algo/Ramping.scala
+++ src/main/scala/hu/szigyi/timelapse/ramping/algo/Ramping.scala
@@ -2,26 +2,47 @@ package hu.szigyi.timelapse.ramping.algo
 
 import hu.szigyi.timelapse.ramping.model.{XMP, XMPSettings}
 
+/**
+  * Equations are based on the wikipedia page of Exposure Value https://en.wikipedia.org/wiki/Exposure_value<br/>
+  * <br/>
+  * EV equation<br/>
+  * EV=log2(N2/t)<br/>
+  * N = relative aperture<br/>
+  * t = shutter speed<br/>
+  */
 class Equations {
 
   import scala.math._
   import math.BigDecimal._
-  import ch.obermuhlner.math.big.BigDecimalMath
+  import ch.obermuhlner.math.big.BigDecimalMath._
   import hu.szigyi.timelapse.ramping.math.BigDecimalConverter._
+  import hu.szigyi.timelapse.ramping.math.BigDecimalDecorator._
 
+  /**
+    * EVdiff=log2(ss1/ss2)<br/>
+    * @param ss1 base shutter speed
+    * @param ss2 current shutter speed
+    * @return EVdiff
+    */
   def shutterSpeeds(ss1: BigDecimal, ss2: BigDecimal): BigDecimal = {
     val div = ss1 / ss2
-    BigDecimalMath.log2(div, defaultMathContext)
+    log2(div, defaultMathContext)
   }
 
+  /**
+    * EVdiff=log2(a1^2^/a2^2^)<br/>
+    * @param a1 base aperture
+    * @param a2 current aperture
+    * @return
+    */
   def apertures(a1: BigDecimal, a2: BigDecimal): BigDecimal = {
-    val div = a1 / a2
-    BigDecimalMath.log(div, defaultMathContext) / log(sqrt(2))
+    val div = a1.`^2` / a2.`^2`
+    log2(div, defaultMathContext)
   }
 
   def ISOs(i1: Int, i2: Int): BigDecimal = {
-    val div = i1 / i2
-    BigDecimalMath.log2(div, defaultMathContext)
+    val div = BigDecimal(i1) / BigDecimal(i2)
+    log2(div, defaultMathContext)
   }
 }
 
@@ -31,15 +52,13 @@ object Equations {
 
 class ExposureAlgorithm(equations: Equations) {
 
-  import hu.szigyi.timelapse.ramping.math.BigDecimalEquals._
-
-  private def neg(bigDecimal: BigDecimal): BigDecimal = bigDecimal * -1
+  import hu.szigyi.timelapse.ramping.math.BigDecimalDecorator._
 
   def shutterSpeeds(standard: XMPSettings, image: XMPSettings): Option[BigDecimal] = {
     if (standard.shutterSpeed === image.shutterSpeed) {
       None
     } else if (standard.shutterSpeed > image.shutterSpeed) {
-      Some(neg(equations.shutterSpeeds(standard.shutterSpeed, image.shutterSpeed)))
+      Some((equations.shutterSpeeds(standard.shutterSpeed, image.shutterSpeed)).neg)
     } else {
       Some(equations.shutterSpeeds(image.shutterSpeed, standard.shutterSpeed))
     }
@@ -54,7 +73,7 @@ class ExposureAlgorithm(equations: Equations) {
     if (standard.iso.equals(image.iso)) {
       None
     } else if (standard.iso > image.iso) {
-      Some(neg(equations.ISOs(standard.iso, image.iso)))
+      Some((equations.ISOs(standard.iso, image.iso)).neg)
     } else {
       Some(equations.ISOs(image.iso, standard.iso))
     }
